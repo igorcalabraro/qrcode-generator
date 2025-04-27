d4d9836295d18b131827e6a1e4193bd6dfd2ade8
Igor <68581590+igorcalabraro@users.noreply.github.com>
2025-04-27 17:34:29 -0300

    feat: qr code response entity route calls service and return mounted response

--- src/main/java/com/igorcalabraro/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/igorcalabraro/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.igorcalabraro.qrcode.generator.controller;
 
 import com.igorcalabraro.qrcode.generator.dto.QrCodeGenerateResponse;
 import com.igorcalabraro.qrcode.generator.dto.QrcodeGenerateRequest;
+import com.igorcalabraro.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeService) {
+        this.qrCodeService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrcodeGenerateRequest request) {
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeService.generateAndUploadQrcode(request.text());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
