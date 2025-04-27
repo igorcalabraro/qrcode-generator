875b3112ee05d4be8feb6526cc72431422a65bb7
Igor <68581590+igorcalabraro@users.noreply.github.com>
2025-04-27 17:29:24 -0300

    feat: init qr code generator service

---
+++ src/main/java/com/igorcalabraro/qrcode/generator/service/QrCodeGeneratorService.java
@@ -0,0 +1,23 @@
+package com.igorcalabraro.qrcode.generator.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.igorcalabraro.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.igorcalabraro.qrcode.generator.ports.StoragePort;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class QrCodeGeneratorService {
+    private final StoragePort storage;
+
+    public QrCodeGeneratorService(StoragePort storage) {
+        this.storage = storage;
+    }
+}
